f5920fbea4a9d5149138ea51f6c300445b072356
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-10-06 13:11:49 -0400

    impl

---
+++ index.js
@@ -0,0 +1,16 @@
+var os = require('os')
+var path = require('path')
+var fs = require('fs-extra')
+var mkdirp = require('mkdirp')
+var guid = require('guid').raw
+
+module.exports = function (work) {
+  var tmpDirHead = guid()
+  tmpDir = path.join(path.join(os.tmpdir(), 'tmp-dir'), tmpDirHead)
+
+  mkdirp.sync(tmpDir)
+
+  work(null, tmpDir, function () {
+    fs.remove(dir)
+  })
+}
